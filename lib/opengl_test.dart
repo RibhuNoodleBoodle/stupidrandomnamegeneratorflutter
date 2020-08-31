840fbb474f54a86381d661fd4300fdb74c152728
ribhutheincompetent <ranjansemail95@gmail.com>
2020-08-31 18:45:41 +0530

    Abandoned Project, Definitely didn't think of the apple kids lol

---
+++ lib/opengl_test.dart
@@ -0,0 +1,18 @@
+import 'package:opengl/opengl.dart';
+
+void main() {
+  // create OpenGL context and make it current
+  //...
+
+  // load OpenGL dynamic library and init all its functions
+  initOpenGL();
+
+  // use OpenGL
+  //...
+  var result = glGetError();
+
+  // glGetError returns GL_NO_ERROR if successful,
+  // or GL_INVALID_OPERATION = 0x0502 (1282) without context in our case
+  print(
+      'glGetError() result is ${result == GL_NO_ERROR ? "GL_NO_ERROR" : result}');
+}
